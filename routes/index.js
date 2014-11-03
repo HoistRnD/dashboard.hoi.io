c258377ad3dfb0c85a5f386ea4b3ca4d447d8e63
Amelia Laundy <amelialaundy@gmail.com>
2014-11-04 11:41:29 +1300

    ip address should be checked correctly

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ var apiCall = new ApiCall();
 /* GET home page. */
 router.get('/', function(req, res) {
   apiCall.getRequest(function (results) {
-    if (req.ip == '116.90.139.105') {
+    var ipAddress = req.headers['X-REAL-IP'] || req.ip;
+    if (ipAddress === '116.90.139.105') {
       res.render('index', { title: 'Dashboard', numEventsTriggered: results['numEventsTriggered'], numModulesExecuted: results['numModulesExecuted']});
     } else {
       res.render('401')
